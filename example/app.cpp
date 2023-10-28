d21d797d6e61c7e740662d1265cacbb8b935b44b
Johan Norberg <johnor@users.noreply.github.com>
2023-10-28 02:53:25 +0200

    Add target to be ignored when running clang-tidy

--- example/app.cpp
+++ example/app.cpp
@@ -1,9 +1,11 @@
 #include "lib.hpp"
+#include "lib_ignored.hpp"
 
 #include <iostream>
 
 int main()
 {
   std::cout << lib_get_greet_for("World") << "\n";
+  std::cout << lib_with_ignored_warnings("Again") << "\n";
   return 0;
 }
