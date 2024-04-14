440f50056823b0f57d7eb988f12f4da57cf7cc87
stevegrossmongodb <136485666+stevegrossmongodb@users.noreply.github.com>
2024-04-13 20:45:07 -0400

    Update run_clang_tidy.sh

--- clang_tidy/run_clang_tidy.sh
+++ clang_tidy/run_clang_tidy.sh
@@ -40,4 +40,4 @@ set -- \
 echo clang command
 echo "${CLANG_TIDY_BIN}" "$@"
 
-"${CLANG_TIDY_BIN}" --config=.clang-tidy "$@" >"$logfile" 2>&1
+"${CLANG_TIDY_BIN}" --config=$(eval - cat ./.clang-tidy) "$@" >"$logfile" 2>&1
